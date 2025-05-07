a580c0935e7559aef886b1c7d7c812066607edb0
ajia0 <928282828@qq.com>
2025-05-07 17:13:11 +0800

    feat: add static

--- test/main.ts
+++ test/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import './style.css'
-import App from './App.vue'
 
 const app = createApp(App)
 app.use(Antd)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  root: "test",
+  build: {
+    outDir: "../dist-static",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "test/index.html"),
+      },
+    },
+  },
+});
